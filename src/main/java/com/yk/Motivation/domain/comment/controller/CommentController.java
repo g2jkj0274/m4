fa9a740569bc9cea763988d5ff87e1f51fde4de9
g2jkj0274 <g2jkj0274@gmail.com>
2023-10-27 00:41:29 +0900

    comment delete fin

--- src/main/java/com/yk/Motivation/domain/comment/controller/CommentController.java
+++ src/main/java/com/yk/Motivation/domain/comment/controller/CommentController.java
@@ -4,9 +4,7 @@ import com.yk.Motivation.domain.comment.entity.Comment;
 import com.yk.Motivation.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/usr/comment")
@@ -20,5 +18,27 @@ public class CommentController {
     public String write(@ModelAttribute Comment comment) {
         commentService.save(comment);
         return "redirect:/usr/article/free1/detail/" + comment.getArticle().getId();  // 댓글을 단 게시글로 리다이렉트
+    }
+
+    // 댓글 수정 페이지로 이동
+    @GetMapping("/modify/{commentId}")
+    public String showModify(@PathVariable long commentId) {
+        return "comment/modify";
+    }
+
+    // 댓글 수정 처리
+    @PostMapping("/modify/{commentId}")
+    public String modify(@PathVariable long commentId, @ModelAttribute Comment comment) {
+        commentService.updateComment(commentId, comment);
+        return "redirect:/usr/article/free1/detail/" + comment.getArticle().getId();
+    }
+
+    // 댓글 삭제 처리
+    @RequestMapping(value = "/delete/{commentId}", method = RequestMethod.GET)
+    public String delete(@PathVariable long commentId) {
+        Comment comment = commentService.findById(commentId);
+        long articleId = comment.getArticle().getId();
+        commentService.delete(commentId);
+        return "redirect:/usr/article/free1/detail/" + articleId;
     }
 }
--- src/main/java/com/yk/Motivation/domain/comment/service/CommentService.java
+++ src/main/java/com/yk/Motivation/domain/comment/service/CommentService.java
@@ -19,4 +19,27 @@ public class CommentService {
     public List<Comment> findByArticleId(long id) {
         return commentRepository.findByArticleId(id);
     }
+
+    // 댓글 수정
+    public void updateComment(long commentId, Comment comment) {
+        Comment existingComment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+
+        existingComment.setContent(comment.getContent());
+        existingComment.setRating(comment.getRating());
+        commentRepository.save(existingComment);
+    }
+
+    // 댓글 삭제
+    public void delete(long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+        commentRepository.delete(comment);
+    }
+
+    // 댓글 조회 (ID 기반)
+    public Comment findById(long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+    }
 }
