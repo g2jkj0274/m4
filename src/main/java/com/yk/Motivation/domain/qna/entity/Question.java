558114ca6f03e0c65fb39b53e6de751447590a2c
g2jkj0274 <g2jkj0274@gmail.com>
2023-10-30 13:22:39 +0900

    comment fin2

--- src/main/java/com/yk/Motivation/domain/qna/entity/Question.java
+++ src/main/java/com/yk/Motivation/domain/qna/entity/Question.java
@@ -1,4 +1,21 @@
+/*
 package com.yk.Motivation.domain.qna.entity;
 
-public class Question {
+import com.yk.Motivation.base.jpa.baseEntity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity  // JPA Entity로 선언
+@Setter  // Lombok: Setter 자동 생성
+@Getter  // Lombok: Getter 자동 생성
+@AllArgsConstructor(access = PROTECTED)  // 모든 필드 값을 인자로 받는 생성자 생성
+@NoArgsConstructor(access = PROTECTED)
+@SuperBuilder  // Lombok: Builder 패턴 구현
+@ToString(callSuper = true)  // Lombok: toString 메서드 오버라이드
+public class Question extends BaseEntity {
+
 }
+*/
