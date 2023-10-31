385557e94d72dff1ef1cb9c14c350de43539fcbb
g2jkj0274 <g2jkj0274@gmail.com>
2023-10-31 13:38:50 +0900

    qna list form

--- src/main/java/com/yk/Motivation/domain/qna/controller/QuestionController.java
+++ src/main/java/com/yk/Motivation/domain/qna/controller/QuestionController.java
@@ -1,4 +1,16 @@
 package com.yk.Motivation.domain.qna.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usr/qna")
+@RequiredArgsConstructor
 public class QuestionController {
+    @GetMapping("/list")
+    public String showList() {
+        return "usr/qna/list";
+    }
 }
--- src/main/java/com/yk/Motivation/domain/qna/entity/Question.java
+++ src/main/java/com/yk/Motivation/domain/qna/entity/Question.java
@@ -1,8 +1,8 @@
-/*
 package com.yk.Motivation.domain.qna.entity;
 
 import com.yk.Motivation.base.jpa.baseEntity.BaseEntity;
-import jakarta.persistence.Entity;
+import com.yk.Motivation.domain.member.entity.Member;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -16,6 +16,22 @@ import static lombok.AccessLevel.PROTECTED;
 @SuperBuilder  // Lombok: Builder 패턴 구현
 @ToString(callSuper = true)  // Lombok: toString 메서드 오버라이드
 public class Question extends BaseEntity {
+    @Column(nullable = false)
+    private int lectureId;  // 강의 ID
+
+    private int curriculumId;  // 커리큘럼 ID
+
+    @Column(nullable = false, length = 255)
+    private String title;  // 질문 제목
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;  // 질문 내용
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;  // 질문을 작성한 사용자
+
+    @Column(nullable = false)
+    private boolean answerStatus = false;  // 답변 상태. 기본값은 false(미등록).
 
 }
-*/
